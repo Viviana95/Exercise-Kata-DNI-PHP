6fc86a4e703fd00717f9fcde4c68e47a3cdd20ba
Viviana <vivitr5195@gmail.com>
2022-08-09 15:20:33 +0200

    first test

--- src/Dni.php
+++ src/Dni.php
@@ -2,10 +2,23 @@
 
 namespace App;
 
+use DomainException;
+
 class Dni {
     
-    
+    private string $dni;   
+    public function construct()
+{
+  return $this->dni="";
+}
 
+public function lengthDni(){
+    
+    if(strlen($this->dni)===9){
+          return "OK";
+    } 
+       
+}
 }
 
 ?>
--- tests/Unit/DniTest.php
+++ tests/Unit/DniTest.php
@@ -2,11 +2,20 @@
 
 namespace Tests;
 
+use App\Dni;
 use PHPUnit\Framework\TestCase;
 
 class DniTest extends TestCase
 {
-    
+   
+    public function test_length_dni()
+    {
+       $dniInsert= new Dni;
+       $dniInsert->construct();
+       $dniCorrect = $dniInsert->lengthDni();
+       $this->assertEquals($dniCorrect, $dniInsert->construct());
+            
+    }
 }
 
 ?>
