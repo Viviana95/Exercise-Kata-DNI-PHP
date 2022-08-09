b51282383a73a80e7f01101e2f399cf686cdcd87
Viviana <vivitr5195@gmail.com>
2022-08-09 16:04:59 +0200

    init second test

--- src/Dni.php
+++ src/Dni.php
@@ -6,10 +6,11 @@ use DomainException;
 
 class Dni {
     
-    private string $dni;   
+    private string $dni=""; 
+
     public function construct()
 {
-  return $this->dni="";
+ // return $this->dni="";
 }
 
 public function lengthDni(){
@@ -17,7 +18,14 @@ public function lengthDni(){
     if(strlen($this->dni)===9){
           return "OK";
     } 
+   
        
+}
+public function charactersNumber(){
+   $numbers= substr($this->dni, 0, -1);
+{
+    
+  }
 }
 }
 
--- tests/Unit/DniTest.php
+++ tests/Unit/DniTest.php
@@ -11,11 +11,18 @@ class DniTest extends TestCase
     public function test_length_dni()
     {
        $dniInsert= new Dni;
-       $dniInsert->construct();
        $dniCorrect = $dniInsert->lengthDni();
        $this->assertEquals($dniCorrect, $dniInsert->construct());
             
     }
+    
+    public function test_first_characters_are_numbers()
+    {
+        $dniInsert= new Dni;
+        $charactersNumber = $dniInsert->charactersNumber();
+        $this->assertEquals($charactersNumber,$dniInsert->construct());
+    }
+    
 }
 
 ?>
